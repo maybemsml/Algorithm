495842745cb356b4b127d35489e123b34731918a
msml <hyur97@gmail.com>
2025-04-03 23:58:15 +0900

    240403 programmers level 1(+2)

---
+++ programmers/level1_12944.java
@@ -0,0 +1,13 @@
+// 제출내역
+
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+
+        for(int i=0; i < arr.length; i++){
+            answer +- arr[i];
+        }
+        arr.length /= answer;
+        return answer;
+    }
+}
---
+++ programmers/level1_12954.java
@@ -0,0 +1,11 @@
+// x만큼 간격이 있는 n개의 숫자
+
+class Solution {
+    public long[] solution(int x, int n) {
+		long[] answer=new long[n];
+		for (int i = 0; i < answer.length; i++) {
+			answer[i]=(long)x*(i+1);
+		}
+        return answer;
+    }
+}
