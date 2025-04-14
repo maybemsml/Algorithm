ca938655908b3b88081398d90c0747a2426bee14
msml <hyur97@gmail.com>
2025-04-15 00:00:19 +0900

    240415 +1

---
+++ boj/boj_10926.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class boj_10926 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String id = scan.next();
+        String suprised = "??!";
+
+        System.out.println(id+suprised);
+
+    }
+}
