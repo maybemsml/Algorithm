18cb439e4ad99aa72743685e490f3e45e4ee8abe
msml <hyur97@gmail.com>
2025-04-19 00:13:26 +0900

    250419 boj+2

---
+++ boj/boj_10171.java
@@ -0,0 +1,9 @@
+// 고양이 만들기... 복사를 잘못했는데 그럴듯하게 목이 긴 고양이처럼 나와서 틀림
+public class boj_10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");   
+        System.out.println(" )  ( ')");   
+        System.out.println("(  /  )");   
+        System.out.println(" \\(__)|");   
+    }
+}
