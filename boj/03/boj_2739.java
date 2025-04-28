4779faad3ec6a7a31fde6edc14185c2e1adb4c33
msml <hyur97@gmail.com>
2025-04-28 23:57:34 +0900

    250428 boj + 1

---
+++ boj/03/boj_2739.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class boj_2739 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.close();
+        
+        for(int i = 1; i < 10; i++) {
+			System.out.println(n+" * "+i+" = "+n*i);
+		}
+    }
+}
