aae4da203502b1d22b4a941b6bcd0727430ad007
msml <hyur97@gmail.com>
2025-04-30 23:42:26 +0900

    250430 boj+1

---
+++ boj/03/boj_24304.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class boj_24304 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int a = scan.nextInt();
+        int sum = 0;
+
+        scan.close();
+        
+        for(int i = 0; i <= a; i++){
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
