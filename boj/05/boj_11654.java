33946a8a8dd9f08fee0ada57b9eabce4f76c6c93
msml <hyur97@gmail.com>
2025-05-21 23:51:43 +0900

    250521 boj+1

---
+++ boj/05/boj_11654.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class boj_11654 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String str = scan.next();
+        scan.close();
+
+        System.out.println((int)str.charAt(0));
+    }
+}
