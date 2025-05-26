dc724ddc1f28a00fa277bbe4d872ca59ee18cbee
msml <hyur97@gmail.com>
2025-05-26 22:21:02 +0900

    250526 boj+1

---
+++ boj/05/boj_25305.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class boj_25305 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+        int k = scan.nextInt();
+
+        int arr[] = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = scan.nextInt();
+        }
+        scan.close();
+
+        Arrays.sort(arr);
+        System.out.println(arr[n-k]);
+    }
+}
--- boj/05/boj_2908.java
+++ boj/05/boj_2908.java
@@ -8,9 +8,9 @@ public class boj_2908 {
         String num2 = scan.next();
 
         scan.close();
-        
+
         num1 = new StringBuilder(num1).reverse().toString();
-        num1 = new StringBuilder(num2).reverse().toString();
+        num2 = new StringBuilder(num2).reverse().toString();
 
         System.out.println(Math.max(Integer.parseInt(num1), Integer.parseInt(num2)));
     }
