6326b5b7b99fea81bc9f424d9e4d98162fcad417
msml <hyur97@gmail.com>
2025-05-30 00:01:56 +0900

    250529 boj+1

---
+++ boj/05/boj_5622.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class boj_5622 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String str = scan.next();
+        int time = 0;
+        int n = str.length();
+        
+        scan.close();
+        
+        for(int i=0; i<n; i++){
+            switch(str.charAt(i)){
+                case 'A' : case 'B' : case 'C' :
+                    time += 3;
+                    break;
+                case 'D' : case 'E' : case 'F' :
+                    time += 4;
+                    break;
+                case 'G' : case 'H' : case 'I' :
+                    time += 5;
+                    break;
+                case 'J' : case 'K' : case 'L' :
+                    time += 6;
+                    break;
+                case 'M' : case 'N' : case 'O' :
+                    time += 7;
+                    break;
+                case 'P' : case 'Q' : case 'R' : case 'S' :
+                    time += 8;
+                    break;
+                case 'T' : case 'U' : case 'V' :
+                    time += 9;
+                    break;
+                case 'W' : case 'X' : case 'Y' : case 'Z' :
+                    time += 10;
+                    break;
+            }
+        }   
+        System.out.println(time);
+    }
+}
